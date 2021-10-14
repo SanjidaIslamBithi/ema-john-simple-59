4883c80ab2a89937debd175d159a4f1f8aeb6996
SanjidaIslamBithi <sanjida333cool@gmail.com>
2021-10-15 00:34:53 +0600

    1st commit

---
+++ src/Firebase/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAnP4lC4CZC7s7i3R4tFJm-bIynNEyRRSQ",
+    authDomain: "ema-john-simple-65461.firebaseapp.com",
+    projectId: "ema-john-simple-65461",
+    storageBucket: "ema-john-simple-65461.appspot.com",
+    messagingSenderId: "448784358727",
+    appId: "1:448784358727:web:28b3a10d359dedc5130062"
+  };
+
+  export default firebaseConfig;
